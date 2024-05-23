02a98df3c7272f90548fddffb8685061a04f4a16
karanpatel0608 <202152315@iiitvadodara.ac.in>
2024-05-24 04:36:16 +0530

    Dismiss by Swiping

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -26,29 +26,47 @@ class _GroceryListState extends State<GroceryList> {
     setState(() {
       _groceryItems.add(newItem);
     });
-    
+  }
+
+  void _removeItem(GroceryItem item) {
+    setState(() {
+      _groceryItems.remove(item);
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-          title: const Text('Your Groceries'),
-          actions: [IconButton(onPressed: _addItem, icon: Icon(Icons.add))]),
-      body: ListView.builder(
+    Widget content = const Center(
+      child: Text('No Items Added Yet!!'),
+    );
+
+    if (_groceryItems.isNotEmpty) {
+      ListView.builder(
         itemCount: _groceryItems.length,
-        itemBuilder: (ctx, index) => ListTile(
-          title: Text(_groceryItems[index].name),
-          leading: Container(
-            width: 24,
-            height: 24,
-            color: _groceryItems[index].category.color,
-          ),
-          trailing: Text(
-            _groceryItems[index].quantity.toString(),
+        itemBuilder: (ctx, index) => Dismissible(
+          onDismissed: (direction) {
+            _removeItem(_groceryItems[index]);
+          },
+          key: ValueKey(_groceryItems[index].id),
+          child: ListTile(
+            title: Text(_groceryItems[index].name),
+            leading: Container(
+              width: 24,
+              height: 24,
+              color: _groceryItems[index].category.color,
+            ),
+            trailing: Text(
+              _groceryItems[index].quantity.toString(),
+            ),
           ),
         ),
-      ),
-    );
+      );
+    }
+
+    return Scaffold(
+        appBar: AppBar(
+            title: const Text('Your Groceries'),
+            actions: [IconButton(onPressed: _addItem, icon: Icon(Icons.add))]),
+        body: content);
   }
 }
