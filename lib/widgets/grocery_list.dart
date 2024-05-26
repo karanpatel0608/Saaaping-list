15bff8e47438273d2f07f7871ce3a658ca64f6c6
karanpatel0608 <202152315@iiitvadodara.ac.in>
2024-05-26 10:11:04 +0530

    Adding a Backend

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -1,7 +1,12 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:shopping_list/data/categories.dart';
 import 'package:shopping_list/data/dummy_items.dart';
+import 'package:shopping_list/models/category.dart';
 import 'package:shopping_list/models/grocery_item.dart';
 import 'package:shopping_list/widgets/new_item.dart';
+import 'package:http/http.dart' as http;
 
 class GroceryList extends StatefulWidget {
   const GroceryList({super.key});
@@ -11,22 +16,66 @@ class GroceryList extends StatefulWidget {
 }
 
 class _GroceryListState extends State<GroceryList> {
-  final List<GroceryItem> _groceryItems = [];
-  void _addItem() async {
-    final newItem = await Navigator.of(context).push<GroceryItem>(
-      MaterialPageRoute(
-        builder: (ctx) => const NewItem(),
-      ),
-    );
+  List<GroceryItem> _groceryItems = [];
 
-    if (newItem == null) {
-      return;
+  @override
+  initState() {
+    super.initState();
+    _loadItems();
+  }
+
+  void _loadItems() async {
+    final url = Uri.https(
+        'flutter-prep-d10e6-default-rtdb.firebaseio.com', 'shopping-list.json');
+    final response = await http.get(url);
+    // print(response);
+      print('Response Status: ${response.statusCode}');
+      print('Response Body: ${response.body}');
+    final Map<String, dynamic> listData = json.decode(response.body);
+
+    final List<GroceryItem> loadedItems = [];
+    for (final item in listData.entries) {
+      final category = categories.entries
+          .firstWhere(
+              (catItem) => catItem.value.title == item.value['category'])
+          .value;
+
+      loadedItems.add(
+        GroceryItem(
+          id: item.key,
+          name: item.value['name'],
+          quantity: item.value['quantity'],
+          category: category,
+        ),
+      );
     }
-
     setState(() {
-      _groceryItems.add(newItem);
+      _groceryItems = loadedItems;
     });
   }
+// ERROR DE RAHA THA KUCH
+  // void _addItem() async {   
+  //   await Navigator.of(context).push<GroceryItem>(
+  //     MaterialPageRoute(
+  //       builder: (ctx) => const NewItem(),
+  //     ),
+  //   );
+
+  //   _loadItems();
+  // }
+
+void _addItem() async {
+  final newItem = await Navigator.of(context).push<GroceryItem>(
+    MaterialPageRoute(
+      builder: (ctx) => const NewItem(),
+    ),
+  );
+
+  // if (newItem != null) {
+    _loadItems();
+  // }
+}
+
 
   void _removeItem(GroceryItem item) {
     setState(() {
@@ -41,7 +90,7 @@ class _GroceryListState extends State<GroceryList> {
     );
 
     if (_groceryItems.isNotEmpty) {
-      ListView.builder(
+      content = ListView.builder(
         itemCount: _groceryItems.length,
         itemBuilder: (ctx, index) => Dismissible(
           onDismissed: (direction) {
